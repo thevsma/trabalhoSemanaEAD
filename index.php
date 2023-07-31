99e88589035b323448af041189e709ea530a6c58
Matheus Vieira <matthewprates1507@gmail.com>
2023-07-31 18:24:51 -0300

    criação da pagina modelo e folha de estilo

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>PAGINA MODELO</title>
 </head>
+
 <body>
-    
+    <header>
+        <h2>DevWeb2023</h2>
+    </header>
+
+    <main>
+        
+    </main>
+
+    <footer>
+        <p>&copy;2023 - Matheus Vieira e Cézar Passos</p>
+    </footer>
 </body>
+
 </html>
